1b96a16ec7c3142c97ce11a774e2dca04696df17
Michael A. Johnson <johnson.michael.andrew@gmail.com>
2019-07-16 22:10:48 -0500

    Show help if executed with no args

--- src/app.ts
+++ src/app.ts
@@ -5,5 +5,8 @@ const cli = new Command();
 const myPackage = require('../package.json'); // TODO: no require here, use a sane loader
 
 cli.version(myPackage.version);
+if (!process.argv.slice(2).length) {
+  cli.outputHelp();
+}
 
 cli.parse(process.argv);
