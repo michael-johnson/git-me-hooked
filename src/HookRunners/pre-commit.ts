e8996f2a5277f1093f8bdcb730f366ce1b31857f
Michael A. Johnson <johnson.michael.andrew@gmail.com>
2019-08-04 13:03:39 -0500

    Added GMH_STAGED_FILES env variable to pre-commit

--- src/HookRunners/pre-commit.ts
+++ src/HookRunners/pre-commit.ts
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
 import { execSync } from 'child_process';
 import { join } from 'path';
+import { env } from 'process';
 
 try {
+  const rawStagedFiles = execSync('git diff --name-only --cached', { encoding: 'utf-8' });
+  const stagedFiles = rawStagedFiles.split('\n').join(',').slice(0, -1); // TODO: cross platform testing
+  env.GMH_STAGED_FILES = stagedFiles;
+
   execSync(`git-me-hooked exec ${join(__dirname, '../../')} pre-commit`, { stdio: 'inherit' });
 } catch (error) {
   process.exit(error.status);
