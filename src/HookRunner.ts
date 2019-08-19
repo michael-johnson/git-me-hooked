5bdd31e5eeb74ec6517afc99be904cc93a9efd25
Michael A. Johnson <johnson.michael.andrew@gmail.com>
2019-08-18 20:04:29 -0500

    Move logic out of hook and into app

--- src/HookRunner.ts
+++ src/HookRunner.ts
@@ -1,7 +1,15 @@
-import { readFileSync } from 'fs';
+import {
+  readFileSync,
+  mkdirSync,
+  existsSync,
+  readdirSync,
+  unlinkSync,
+} from 'fs';
 import { join, parse, resolve } from 'path';
 import { exec } from 'shelljs';
 import { chdir } from 'process';
+import HookVariableInitializer from './HookVariableInitializer';
+import HookManager from './HookManager';
 
 export default class HookRunner {
   protected commands: ExecConfig = {};
@@ -9,6 +17,9 @@ export default class HookRunner {
   public run(repoPath: string, hookType: string): number {
     let responseCode = 0;
     this.commands = {};
+
+    HookRunner.initTempDirectory();
+    HookVariableInitializer.initEnvVariables(repoPath, hookType);
 
     const repoConfig = HookRunner.readConfigFile(join(repoPath, 'git-me-hooked.json'));
     this.getIncludes(repoConfig, repoPath, hookType);
@@ -18,13 +29,32 @@ export default class HookRunner {
       chdir(path);
       const commands = this.commands[key];
       commands.forEach(command => {
-        if (exec(command.exec).code !== 0) {
+        const res = exec(command.exec);
+        if (res.code !== 0) {
           responseCode = 1;
         }
       });
     });
 
+    HookRunner.cleanTempDirectory();
+
     return responseCode;
+  }
+
+  protected static initTempDirectory(): void {
+    if (!existsSync(HookManager.getTempDirectory())) {
+      mkdirSync(HookManager.getTempDirectory());
+    }
+  }
+
+  protected static cleanTempDirectory(): void {
+    const tempDirectory = HookManager.getTempDirectory();
+    if (existsSync(tempDirectory)) {
+      const files = readdirSync(tempDirectory);
+      files.forEach(file => {
+        unlinkSync(join(tempDirectory, file));
+      });
+    }
   }
 
   protected getIncludes(repoConfig: GitMeHookedConfig, currentPath: string, hookType: string) {
