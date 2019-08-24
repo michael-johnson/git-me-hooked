e92f46d4000e759fd1b4368fca5e220055d54016
Michael A. Johnson <johnson.michael.andrew@gmail.com>
2019-08-24 12:51:39 -0500

    Add more HookManager tests

--- src/__tests__/HookManager.test.ts
+++ src/__tests__/HookManager.test.ts
@@ -1,4 +1,4 @@
-import { mkdirSync, readFileSync } from 'fs';
+import { mkdirSync, readFileSync, writeFileSync } from 'fs';
 import { join } from 'path';
 import { tmpdir } from 'os';
 import { chdir } from 'process';
@@ -6,17 +6,25 @@ import { exec } from 'shelljs';
 import rimraf from 'rimraf';
 
 import HookManager from '../HookManager';
+import { ERROR_NOT_GIT_REPO, ERROR_NOT_INITIALIZED } from '../strings/errors';
 
 const tempRepoPath = join(tmpdir(), 'gmh-test-repo');
 const hookTemplatePath = join(__dirname, '../hookTemplate.ts');
 
-beforeAll(() => {
+jest.spyOn(global.console, 'log').mockImplementation(() => {});
+
+const cleanUp = () => {
   // Setup empty git repo for testing
   rimraf.sync(tempRepoPath);
   mkdirSync(tempRepoPath);
   chdir(tempRepoPath);
   exec('git init');
+};
+
+beforeAll(() => {
+  cleanUp();
 });
+
 
 it('init copies hooks', () => {
   // Act
@@ -27,4 +35,52 @@ it('init copies hooks', () => {
   const hookTemplate = readFileSync(hookTemplatePath, { encoding: 'utf-8' });
   const preCommitHookRunner = hookTemplate.replace('%%_HOOK_NAME_%%', 'pre-commit');
   expect(installedPreCommitHook).toEqual(preCommitHookRunner);
+});
+
+it('init gives error if not git repo', () => {
+  // Arrange
+  const nonRepoPath = join(tmpdir(), 'gmh-not-a-repo');
+
+  // Act
+  HookManager.init(nonRepoPath);
+
+  // Assert
+  expect(console.log).toBeCalledWith(ERROR_NOT_GIT_REPO);
+});
+
+
+it('uninit re-installs backed up hook', () => {
+  // Arrange
+  const ORIG_HOOK_CONTENTS = 'Blah';
+  writeFileSync(join(tempRepoPath, '.git', 'hooks', 'pre-commit'), ORIG_HOOK_CONTENTS); // write "Blah" pre-commit hook
+  HookManager.init(tempRepoPath); // Init GMH, should back up orig hook
+
+  // Act
+  HookManager.uninit(tempRepoPath);
+
+  // Assert: original hook should be installed again
+  const installedPreCommitHook = readFileSync(join(tempRepoPath, '.git', 'hooks', 'pre-commit'), { encoding: 'utf-8' });
+  expect(installedPreCommitHook).toEqual(ORIG_HOOK_CONTENTS);
+});
+
+it('uninit gives error if not git repo', () => {
+  // Arrange
+  const nonRepoPath = join(tmpdir(), 'gmh-not-a-repo');
+
+  // Act
+  HookManager.uninit(nonRepoPath);
+
+  // Assert
+  expect(console.log).toBeCalledWith(ERROR_NOT_GIT_REPO);
+});
+
+it('uninit gives error if git repo, but not initialized by GMH', () => {
+  // Arrange
+  cleanUp();
+
+  // Act
+  HookManager.uninit(tempRepoPath);
+
+  // Assert
+  expect(console.log).toBeCalledWith(ERROR_NOT_INITIALIZED);
 });
---
+++ src/strings/errors.ts
@@ -0,0 +1,2 @@
+export const ERROR_NOT_GIT_REPO = 'The given directory is not a git repository.';
+export const ERROR_NOT_INITIALIZED = 'The given directory does not have a git-me-hooked.json file in it.';
